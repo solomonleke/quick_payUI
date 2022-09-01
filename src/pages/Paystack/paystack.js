ba5f9561ac7481bb705a7d6d230faff625bd54b5
Komatech <kelechi.akoma@yahoo.com>
2022-09-01 01:06:53 +0100

    fix

--- src/pages/Paystack/paystack.js
+++ src/pages/Paystack/paystack.js
@@ -1,13 +1,21 @@
 import { usePaystackPayment } from 'react-paystack';
 import { useState } from 'react';
-
+import { useNavigate } from 'react-router-dom';
 
 
 export default function PayStack(){
     const amount = sessionStorage.getItem('amount');
     const email = sessionStorage.getItem('email');
 
+    const name = sessionStorage.getItem('name');
+    const meter = sessionStorage.getItem('category');
+    const meter_no = sessionStorage.getItem('meter_no');
+    const number = sessionStorage.getItem('account');
+
     const [state,setState] = useState(false);
+
+    const navigate = useNavigate()
+
     const config = {
         reference: (new Date()).getTime().toString(),
         email: email,
@@ -29,6 +37,31 @@ export default function PayStack(){
         const response = await fetch(url,other)
         const data = await response.json()
         console.log(data)
+        if(data.data.data.status=="success"){
+            const url = "https://quikpayapi.smartpowerbilling.com/payment";
+            const other = {
+                method: 'POST',
+                body: JSON.stringify(options(meter_no,meter,amount,name)),
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            }
+        
+            fetch(url,other)
+            .then((response) => response.json())
+            .then((data) => {alert(data.message)
+                                console.log(data)
+                                sessionStorage.setItem('limit_amount', data.data.vendorBal);
+                                sessionStorage.setItem('token_id', data.data.token);
+                                sessionStorage.setItem('unit', data.data.unit);
+                                // sessionStorage.setItem('amount', data.amount);
+                                sessionStorage.setItem('vendor', data.data.vendorName);
+                                sessionStorage.setItem('arrears', data.data.arrears);
+                                navigate('/checkout')
+                            })
+            .catch((error) => console.log(error));
+            setState(true)
+        }
     };
 
     const sendSuccess = (reference) =>{
@@ -39,6 +72,9 @@ export default function PayStack(){
     const onClose = () => {
       // implementation for  whatever you want to do when the Paystack dialog closed.
       console.log('closed')
+      if(state==true){
+        navigate("/checkout")
+      }
     }
     
     const initializePayment = usePaystackPayment(config);
