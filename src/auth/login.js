bbadd886425e9b04b5d4023a163c28f5b5f25f6d
odenigbo Godfrey <odenigbogodfrey@yahoo.com>
2022-09-06 19:14:08 +0100

    view-container-index

--- src/auth/login.js
+++ src/auth/login.js
@@ -6,6 +6,7 @@ import { request } from "./interlude";
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { showToast } from "../utility/tool";
+import View from "./view";
 
 export default function SigninContent(){
     const [username,setUsername]=useState('');
@@ -18,7 +19,9 @@ export default function SigninContent(){
     // const url = 'https://jsonplaceholder.typicode.com/users';
     const method = "POST"
 
-    function Click(){
+    function Click({
+        username, password
+    }){
         var data = JSON.stringify({
           "table": "tbl_cashcollect_users",
           "username": username,
@@ -81,45 +84,7 @@ export default function SigninContent(){
     
 
 
-    return(
-        <div className="authincation h-100">
-        <div className="container h-100">
-            <div className="row justify-content-center h-100 align-items-center">
-                <div className="col-md-6">
-                    <div className="authincation-content">
-                        <div className="row no-gutters">
-                            <div className="col-xl-12">
-                                <div className="auth-form">
-                                    <h4 className="text-center mb-4">Sign in to your account</h4>
-                                    <form action="" onSubmit={(e)=>{e.preventDefault();Click()}}>
-                                        <div className="form-group">
-                                            <label className="mb-1"><strong>Username</strong></label>
-                                            <input type="Username" className="form-control"  onChange={e=>setUsername(e.target.value)}></input>
-                                        </div>
-                                        <div className="form-group">
-                                            <label className="mb-1"><strong>Password</strong></label>
-                                            <input type="Password" className="form-control"  onChange={e=>setPassword(e.target.value)}></input>
-                                        </div>
-                                        <div className="form-row d-flex justify-content-between mt-4 mb-2">
-                                            
-                                            <div className="form-group">
-                                                <a href="">Forgot Password?</a>
-                                            </div>
-                                        </div>
-                                        <div className="text-center">
-                                            <button className="btn btn-primary btn-block" type="submit" style={{background:"#017cc2"}}  >Sign me In</button>
-                                        </div>
-                                        
-                                            <span>{isLoggedIn}</span>
-                                        
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    )
+    return (
+        <View Click={({username, password}) => Click({username, password})} isLoggedIn={isLoggedIn} />
+    );
 }
