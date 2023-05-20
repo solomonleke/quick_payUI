d112edd0bc68f63aef35f7083300a9be851b2eca
Komatech <kelechi.akoma@yahoo.com>
2023-05-20 14:11:00 +0100

    Sub account

--- src/pages/Vendor wallet/vendorPay.js
+++ src/pages/Vendor wallet/vendorPay.js
@@ -80,6 +80,7 @@ export default async function pay(amount,bill,acc_no,name,meter_no,payment){
         sessionStorage.setItem('vendor', data.data.vendorName);
         sessionStorage.setItem('arrears', data.data.arrears);
         sessionStorage.setItem('vat', data.data.vat);
+        sessionStorage.setItem('buckets',JSON.stringify(data.data.buckets))
         showToast({
             message: data.message,
             type: 'success'
